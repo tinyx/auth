8a6a74e36460ccf44d068e7cf046a2aa889107a8
Yitao Xiong <hb1412@gmail.com>
2016-12-11 14:54:33 -0500

    Added antd

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Login from './components/Login';
-import Register from './components/Register';
+import Login from './views/Login';
+import Register from './views/Register';
 import { Router, Route, browserHistory } from 'react-router'
 import './index.css';
 
