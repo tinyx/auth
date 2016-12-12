2ffcc9af1d825f75c93cd9b1e4a5d10a87a3ff32
Yitao Xiong <yitao@bitsighttech.com>
2016-12-12 16:30:16 -0500

    Register and login are both working

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,15 +1,53 @@
 import React from 'react'
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import { Form, Icon, Input, Button, Checkbox, message } from 'antd';
+import { Link } from 'react-router';
 const FormItem = Form.Item;
+import { checkHttpStatus, parseJSON } from '../utils.js';
 import 'antd/dist/antd.css';
 import './login.css';
 
 const Login = Form.create()(React.createClass({
   handleSubmit(e) {
     e.preventDefault();
+    let redirectUrl = null;
+    if('redirect' in this.props.location.query) {
+      redirectUrl = this.props.location.query.redirect;
+    }
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        console.log('Received values of form: ', values);
+        fetch('http://crabfactory.net/api-token-auth/', {
+          method: 'post',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({username: values.userName, password: values.password})
+        })
+        .then(checkHttpStatus)
+        .then(parseJSON)
+        .then(response => {
+          if(response.token) {
+            let token = response.token;
+            // set the cookie
+            document.cookie = "jwt_token=" + token;
+
+            if(redirectUrl) {
+              // perform redirect
+              setTimeout( function(token){
+                  window.location = redirectUrl;
+              }.bind(null, token), 500);
+            }
+            else {
+              message.info('Login successfully!', 5);
+            }
+          }
+          else {
+            message.error('Please check the username and password and try again.', 5);
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        })
       }
     });
   },
@@ -42,7 +80,7 @@ const Login = Form.create()(React.createClass({
           <Button type="primary" htmlType="submit" className="login-form-button">
             Log in
           </Button>
-          Or <a>register now!</a>
+          Or <Link to={{ pathname: '/register', query: this.props.location.query }}>register now!</Link>
         </FormItem>
       </Form>
     );
--- src/views/Register.js
+++ src/views/Register.js
@@ -1,11 +1,165 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Form, Input, Button, message } from 'antd';
+const FormItem = Form.Item;
+import { checkHttpStatus, parseJSON } from '../utils.js';
+import './register.css';
 
-class Register extends Component {
+const Register = Form.create()(React.createClass({
+  getInitialState() {
+    return {
+      passwordDirty: false,
+    };
+  },
+  handleSubmit(e) {
+    e.preventDefault();
+    let redirectUrl = null;
+    if('redirect' in this.props.location.query) {
+      redirectUrl = this.props.location.query.redirect;
+    }
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        fetch('http://crabfactory.net/api-token-register/', {
+          method: 'post',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            username: values.userName,
+            password: values.password,
+            email: values.email,
+          })
+        })
+        .then(checkHttpStatus)
+        .then(parseJSON)
+        .then(response => {
+          if(response.token) {
+            let token = response.token;
+            // set the cookie
+            document.cookie = "jwt_token=" + token;
+
+            if(redirectUrl) {
+              // perform redirect
+              setTimeout( function(token){
+                  window.location = redirectUrl;
+              }.bind(null, token), 500);
+            }
+            else {
+              message.info('Register successfully!', 5);
+            }
+          }
+          else {
+            message.error('Please check the inputs and try again.', 5);
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        })
+      }
+    });
+  },
+  handlePasswordBlur(e) {
+    const value = e.target.value;
+    this.setState({ passwordDirty: this.state.passwordDirty || !!value });
+  },
+  checkPassowrd(rule, value, callback) {
+    const form = this.props.form;
+    if (value && value !== form.getFieldValue('password')) {
+      callback('Two passwords that you enter is inconsistent!');
+    } else {
+      callback();
+    }
+  },
+  checkConfirm(rule, value, callback) {
+    const form = this.props.form;
+    if (value && this.state.passwordDirty) {
+      form.validateFields(['confirm'], { force: true });
+    }
+    callback();
+  },
   render() {
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      labelCol: { span: 6 },
+      wrapperCol: { span: 14 },
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        span: 14,
+        offset: 6,
+      },
+    };
     return (
-      <div>Register Page</div>
+      <Form horizontal onSubmit={this.handleSubmit} className="register-form">
+        <FormItem
+          {...formItemLayout}
+          label="User Name"
+          hasFeedback
+        >
+          {getFieldDecorator('userName', {
+            rules: [{ required: true, message: 'Please input your user name!' }],
+          })(
+            <Input />
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label="Password"
+          hasFeedback
+        >
+          {getFieldDecorator('password', {
+            rules: [{
+              required: true, message: 'Please input your password!',
+            }, {
+              validator: this.checkConfirm,
+            }],
+          })(
+            <Input type="password" onBlur={this.handlePasswordBlur} />
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label="Confirm Password"
+          hasFeedback
+        >
+          {getFieldDecorator('confirm', {
+            rules: [{
+              required: true, message: 'Please confirm your password!',
+            }, {
+              validator: this.checkPassowrd,
+            }],
+          })(
+            <Input type="password" />
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label="E-mail"
+          hasFeedback
+        >
+          {getFieldDecorator('email', {
+            rules: [{
+              type: 'email', message: 'The input is not valid E-mail!',
+            }, {
+              required: true, message: 'Please input your E-mail!',
+            }],
+          })(
+            <Input />
+          )}
+        </FormItem>
+        <FormItem {...tailFormItemLayout}>
+          <Button
+            type="primary"
+            htmlType="submit"
+            size="large"
+            className="register-form-button"
+          >
+            Register
+          </Button>
+        </FormItem>
+      </Form>
     );
-  }
-}
+  },
+}));
 
 export default Register;
